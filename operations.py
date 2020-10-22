6bafbe8d0f76c2f39362ad13a198c5fd72881090
Batishchev Pavel <batishchev.pavel@gmail.com>
2020-10-22 14:36:10 +0300

    .

--- operations.py
+++ operations.py
@@ -22,6 +22,7 @@ def clean_old(disk):
             print("Error: %s : %s" % (f, e.strerror))
 
 def copy_new(disk):
+    # Распаковываем архив, если ещё не распаковали
     if not os.path.exists('new_maps'):
         new_maps = glob.glob('*.zip')
 
@@ -32,6 +33,7 @@ def copy_new(disk):
         with zipfile.ZipFile(new_maps[0], 'r') as zip_ref:
             zip_ref.extractall('new_maps')
 
+    # Копируем GPX
     target_gpx = pathlib.Path(disk + ':/', 'Garmin', 'GPX')
     target_gpx.mkdir(parents=True, exist_ok=True)
     for f in glob.glob(os.path.join('new_maps', 'Garmin', 'GPX', '*.gpx')):
@@ -41,25 +43,34 @@ def copy_new(disk):
     target_kmz = pathlib.Path(disk + ':/', 'Garmin', 'CustomMaps')
     target_kmz.mkdir(parents=True, exist_ok=True)
 
+    # Копируем KMZ
     source_kmz = glob.glob(os.path.join('new_maps', 'Garmin', 'CustomMaps', '*.kmz'))
     while len(source_kmz) > 1:
-        print('Нашли несколько kmz')
+        print('\nНашли несколько kmz')
         for i in range(len(source_kmz)):
             print('%i - %s' % (i+1, source_kmz[i]))
+        print('0 - пропустить')
 
         try:
             index = int(input('Выберите нужный kmz: '))
+
+            if index == 0:
+                source_kmz = []
+                break
 
             if index < 1 or index > len(source_kmz):
                 continue
 
             source_kmz = [source_kmz[index-1]]
+            break
         except:
             continue
-    source_kmz = source_kmz[0]
-    print('Копируем %s' % source_kmz)
-    shutil.copy(source_kmz, target_kmz)
 
+    for f in source_kmz:
+        print('Копируем %s' % f)
+        shutil.copy(f, target_kmz)
+
+    # Копируем JNX
     target_jnx = pathlib.Path(disk + ':/', 'Garmin', 'BirdsEye')
     target_jnx.mkdir(parents=True, exist_ok=True)
     for f in glob.glob(os.path.join('new_maps', 'Garmin', 'BirdsEye', '*.jnx')):
